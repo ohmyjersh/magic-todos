9f426e39632eefdaacad78719376571eda325bb8
Josh Jarmain <jjarmain@newrelic.com>
2019-02-11 08:56:32 -0800

    working

--- apps/fyre/lib/kafka_observer.ex
+++ apps/fyre/lib/kafka_observer.ex
@@ -9,25 +9,6 @@ defmodule KafkaObserver do
     Task.async(fn -> async_message_handler() end)
     {:ok, state}
   end
-  # def handle_cast({:start_consumer}, state) do
-  #   start_consuming()
-  #   {:noreply, state }
-  # end
-  # def handle_cast({:stop_consumer}, state) do
-  #   stop_consuming()
-  #   {:noreply, state }
-  # end
-
-  # defp start_consuming() do
-  #   KafkaEx.create_worker(:streaming_worker)
-  #   for message <- KafkaEx.stream("test", 0, worker_name: :streaming_worker) do
-  #     json = Poison.decode!(message.value)
-  #     GenServer.cast(:collector, {:handle_messages, json})
-  #   end
-  # end
-  # def stop_consuming() do
-  #   KafkaEx.stop(:streaming_worker)
-  # end
 
   defp async_message_handler() do
     KafkaEx.create_worker(:streaming_worker)
--- apps/fyre/lib/validator.ex
+++ apps/fyre/lib/validator.ex
@@ -17,17 +17,21 @@ defmodule Validator do
       false -> false
     end
   end
+  def value_is_greater_than(value, greater_than), do: value > greater_than
   def run_over_max_runtime(test_start_time, max_runtime) do
-    case diff_times(test_start_time, Time.utc_now()) > max_runtime do
+    case value_is_greater_than(diff_times(test_start_time, Time.utc_now()), max_runtime) do
       true -> true
       false -> false
     end
   end
 
-  def has_all_requests_messages(current_request_length, current_message_length, max_length) do
-    isSameLength(current_request_length, max_length) && isSameLength(current_message_length, max_length)
-  end
+  def is_messages_greater_than_requests(message_length, request_length),
+    do: value_is_greater_than(message_length, request_length)
 
+  def has_all_requests_messages(current_request_length, current_message_length, max_length),
+    do: isSameLength(current_request_length, max_length) && isSameLength(current_message_length, max_length)
+
+  #refactor, please halp
   defp checker(current_request_length, current_message_length, max_length, test_start_time, max_runtime) do
     IO.puts "checking"
     if has_all_requests_messages(current_request_length, current_message_length, max_length) do
@@ -36,39 +40,36 @@ defmodule Validator do
       IO.puts current_request_length
       IO.puts "MESSAGES:"
       IO.puts current_message_length
+      time = get_request_execution_time(test_start_time, Time.utc_now())
+      IO.puts "Execution TIME"
+      IO.inspect time
       true
     else
       if run_over_max_runtime(test_start_time, max_runtime) do
-        IO.puts "oops ran outta time"
+        IO.puts "FAILED"
+        IO.puts "Execution time succeeded the maximum runtime of #{max_runtime} seconds"
         false
       else
-        IO.puts "gotta check again"
-        checker(get_requests_length(), get_messages_length(), max_length, test_start_time, max_runtime)
+        if is_messages_greater_than_requests(current_message_length, current_request_length) do
+          IO.puts "FAILED"
+          IO.puts "Recieved messages: #{current_message_length} and requests: #{current_request_length}"
+          IO.puts "protip: Clear kafka topic and try again."
+          false
+        else
+          IO.puts "gotta check again"
+          checker(get_requests_length(), get_messages_length(), max_length, test_start_time, max_runtime)
+        end
       end
     end
   end
 
-  #kafkaobserver
-  # defp start_consumer() do
-  #   GenServer.cast(:kafkaobserver, {:start_consumer})
-  # end
-  # defp stop_consumer() do
-  #   GenServer.cast(:kafkaobserver, {:stop_consumer})
-  # end
-
   #collector
-  defp get_messages_length() do
-    messages = GenServer.call(:collector, :get_messages)
-    length(messages)
-  end
-  defp get_requests_length() do
-    requests = GenServer.call(:collector, :get_requests)
-    length(requests)
-  end
-  defp get_times() do GenServer.call(:collector, :get_request_times) end
-  defp get_request_execution_time(start_time, end_time) do
-    diff_times(start_time, end_time)
-  end
+  defp get_messages_length(), do: GenServer.call(:collector, :get_messages) |> length
+  defp get_requests_length(), do: GenServer.call(:collector, :get_requests) |> length
+  defp get_times(), do: GenServer.call(:collector, :get_request_times)
+
+
+  defp get_request_execution_time(start_time, end_time), do: diff_times(start_time, end_time)
   defp diff_times(start_time, end_time), do: Time.diff(end_time, start_time)
 
   def call_all_the_things(args) do
@@ -77,11 +78,9 @@ defmodule Validator do
     messages_length = get_messages_length()
     times = get_times();
 
+    #possibly do a check to see if app is still sending requests, if so do not start checker
     #run checker
     checker(request_length, messages_length, args.max_length, times.start, args.max_runtime)
-    time = get_request_execution_time(times.start, Time.utc_now())
-    IO.puts "Execution TIME"
-    IO.inspect time
     {:ok}
   end
 end
